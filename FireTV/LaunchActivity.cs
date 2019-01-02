63d36bf19f03718945938eb1cddccdc9d3d68d29
Daniel Hazelbaker <daniel@blueboxmoon.com>
2019-01-02 14:41:43 -0800

    - Initial import

---
+++ FireTV/LaunchActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace com.shepherdchurch.TVApp
+{
+    [Activity( Label = "@string/app_name", MainLauncher = true )]
+    [IntentFilter( new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLeanbackLauncher } )]
+    public class LaunchActivity : Activity
+    {
+        protected override void OnCreate( Bundle savedInstanceState )
+        {
+            base.OnCreate( savedInstanceState );
+
+            Crex.Android.Application.Current.Run( this );
+        }
+    }
+}
---
+++ Roku/components/readme.txt
@@ -0,0 +1,4 @@
+You need to download the crex-roku package from
+https://github.com/BlueBoxMoon/crex-roku
+and place the contents into the crex folder. The crex folder
+itself should contain a number of files, including crex.root.txt
