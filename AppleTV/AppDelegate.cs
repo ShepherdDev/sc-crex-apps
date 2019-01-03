fa0984739b6856b71fcb2df0a0adf180d0d152a8
Daniel Hazelbaker <daniel@blueboxmoon.com>
2019-01-03 10:01:05 -0800

    + Added Apple TV application.

---
+++ AppleTV/AppDelegate.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using UIKit;
+
+namespace com.shepherdchurch.ATVApp
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register( "AppDelegate" )]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching( UIApplication application, NSDictionary launchOptions )
+        {
+            Window = new UIWindow( UIScreen.MainScreen.Bounds );
+
+            Crex.tvOS.Application.Current.Run( Window );
+
+            return true;
+        }
+    }
+}
